e14a4d24d647f744d01a96c5009a1a2e7947f67b
Ayush <ayushratan082@gmail.com>
2024-07-12 11:08:17 +0530

    pages added

--- pages/mode.tsx
+++ pages/mode.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import Header from '../components/Header';
-import MainContent from '../components/MainContent';
+import MainContent from '../components/MainContent_mode';
 import Footer from '../components/Footer';
+
+
 
 const Home = () => {
     return (
