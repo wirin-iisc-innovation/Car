74949fb33ebd5f3735a6195ca89506aec13b45ab
AkZuza <akhiljose@gmail.com>
2024-07-12 16:49:24 +0530

    add QuickControls page

---
+++ pages/settings/QuickControlsPage.tsx
@@ -0,0 +1,94 @@
+// pages/dashboard.js
+"use client";
+
+import styled from 'styled-components';
+import { useState } from 'react';
+import { Button } from '../../components/Button';
+import { GroupButtonItem } from '../../components/GroupButtonItem';
+import SettingsLabel from '../../components/SettingsLabel';
+
+const Container = styled.div`
+  display: flex;
+  height: 434px;
+  width: 1964px;
+`;
+
+const Sidebar = styled.div`
+  width: 483px;
+  height: 434px;
+  background: #1c1c1c;
+  color: #fff;
+  padding: 20px;
+`;
+
+const MainContent = styled.div`
+  flex: 1;
+  background: #000;
+  color: #fff;
+  padding: 20px;
+  flex-direction: row;
+`;
+
+const MenuItem = styled.div`
+  display: flex;
+  align-items: center;
+  height: 48px;
+  padding: 10px 0;
+  cursor: pointer;
+
+  font-family: 'Urbanist';
+  font-weight: 600;
+  font-size: 25px;
+  color: #737373;
+
+  &:hover {
+    background: #333;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+const ContentSection = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Slider = styled.input`
+  width: 100%;
+`;
+
+
+const ButtonGroup = styled.div`
+  width: 680px;
+  height: 48px;
+  background-color: #333;
+
+  border-radius: 60px;
+`;
+
+
+const QuickControlsSettingsPage = () => {
+
+
+  return (
+    <Container>
+        <MainContent>
+            <ContentSection>
+                <SettingsLabel title='Display Brightness'/>
+            </ContentSection>
+
+            <ContentSection>
+                <SettingsLabel title='Exterior Lights'/>
+            </ContentSection>
+
+            <ContentSection>
+                <SettingsLabel title='Adjustments'/>
+            </ContentSection>
+        </MainContent>
+    </Container>
+  );
+};
+
+export default QuickControlsSettingsPage;
+  
