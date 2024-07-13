efe720ba6ca35146618a399331d3d4305f67f2be
su kr <sumadhvakrishna2005@gmail.com>
2024-07-13 13:48:00 +0530

    alert

--- components/alert_comp.tsx
+++ components/alert_comp.tsx
@@ -9,36 +9,25 @@ const MainContent = () => {
                 <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
             </Head>
             <main className="main-content">
-                <p className="select-mode-text">Switch Car Mode</p>
+            <p className="select-mode-text">Switch Car Mode</p>
+                
                 <div className="mode-buttons">
-                  
+                    <Link href="/start1">
                         <div className="mode-button">
-                            <img src="/images/Drive Glow.svg" alt="Drive Icon" />
+                            <img src="/images_mode/Drive Glow.svg" alt="Drive Icon" />
                         </div>
-                  
+                    </Link>
                     <div className="mode-button">
-                        <img src="/images/parked.svg" alt="Parked Icon" />
+                        <img src="/images_mode/parked.svg" alt="Parked Icon" />
                     </div>
-               
+                    <Link href="/start">
                         <div className="mode-button">
-                            <img src="/images/auto.svg" alt="Auto Icon" />
+                            <img src="/images_mode/auto.svg" alt="Auto Icon" />
                         </div>
-                   
+                    </Link>
                 </div>
-                <div className="battery-info-bar">
-                    <div className="battery-info">
-                        <p className="remaining-km">232 km</p>
-                        <p className="unit remaining-text">Remaining</p>
-                        <p className="battery-percentage">97%</p>
-                        <p className="unit battery-text">Battery</p>
-                        <p className="avg-wh">110 Wh/km</p>
-                        <p className="unit average-text">Average</p>
-                    </div>
-                    <div className="battery-bar-container">
-                        <div className="battery-bar-base"></div>
-                        <div className="battery-bar-fill"></div>
-                    </div>
-                </div>
+                
+                
             </main>
         </>
     );
