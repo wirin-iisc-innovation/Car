6b17c6b9014e9228d1a0f2a1eef0497a1575e9f4
Ayush <ayushratan082@gmail.com>
2024-07-14 02:14:05 +0530

    min

--- pages/app.tsx
+++ pages/app.tsx
@@ -4,6 +4,7 @@ import Footer from '../components/Footer'; // Adjust the path as needed
 import Music from '../pages/music'; // Adjust the path as needed
 import Maps from '../pages/maps'; // Adjust the path as needed
 import Dashboard from '../pages/dashboard'; 
+import Mode from '../pages/mode'; 
 // import other pages as needed
 
 const App: React.FC = () => {
@@ -13,7 +14,8 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/page1" element={<Music />} />
           <Route path="/page2" element={<Maps />} />
-          <Route path="/page2" element={<Dashboard />} />
+          <Route path="/page3" element={<Dashboard />} />
+          <Route path="/page4" element={<Mode />} />
           {/* Add more routes as needed */}
         </Routes>
         <Footer />
