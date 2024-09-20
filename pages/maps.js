6173c7a8141e45a4d2f0cf2ec2beab50bd857e31
Ayush <ayushratan082@gmail.com>
2024-09-21 00:21:03 +0530

    first

--- pages/maps.js
+++ pages/maps.js
@@ -9,41 +9,7 @@ import Speedometer from "../components/speed";
 export default function Home() {
   useEffect(() => {
     // Update the time and date
-
     // Update the weather information
-
-    // Handle resizing
-    const mapContainer = document.querySelector(".map-container");
-    const resizeHandle = document.querySelector(".resize-handle");
-
-    let isResizing = false;
-
-    const handleMouseDown = (event) => {
-      isResizing = true;
-      document.addEventListener("mousemove", handleMouseMove);
-      document.addEventListener("mouseup", handleMouseUp);
-    };
-
-    const handleMouseMove = (event) => {
-      if (!isResizing) return;
-      const newWidth =
-        event.clientX - mapContainer.getBoundingClientRect().left;
-      mapContainer.style.width = `${newWidth}px`;
-    };
-
-    const handleMouseUp = () => {
-      isResizing = false;
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
-    };
-
-    resizeHandle.addEventListener("mousedown", handleMouseDown);
-
-    return () => {
-      resizeHandle.removeEventListener("mousedown", handleMouseDown);
-      document.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleMouseUp);
-    };
   }, []);
 
   return (
@@ -63,7 +29,6 @@ export default function Home() {
               frameBorder="0"
               allowFullScreen
             ></iframe>
-            <div className="resize-handle"></div>
           </div>
         </div>
       </main>
